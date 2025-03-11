06cdbdd0a8b5880719636483412c6ca3166f650d
Trishane Reid <trishanereid17@gmail.com>
2025-03-11 21:20:44 +0530

    initial commit

---
+++ backend/debt-management-api/src/main/java/com/debtmanager/debt_management_api/DebtManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.debtmanager.debt_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtManagementApiApplication.class, args);
+	}
+
+}
---
+++ backend/debt-management-api/src/test/java/com/debtmanager/debt_management_api/DebtManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debtmanager.debt_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
