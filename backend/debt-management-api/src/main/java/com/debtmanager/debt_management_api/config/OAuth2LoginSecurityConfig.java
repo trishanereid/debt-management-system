c3852221398586b636377c98c2b6b0c6e1138cc0
Trishane Reid <trishanereid17@gmail.com>
2025-03-17 20:29:47 +0530

    Modify security configuration

--- backend/debt-management-api/src/main/java/com/debtmanager/debt_management_api/config/OAuth2LoginSecurityConfig.java
+++ backend/debt-management-api/src/main/java/com/debtmanager/debt_management_api/config/OAuth2LoginSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 @Configuration
 @EnableWebSecurity
@@ -14,14 +16,16 @@ public class OAuth2LoginSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/login/**", "/error", "/webjars/**").permitAll()
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/user/retrieve/{id}").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/home",true)
-                );
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+//                .oauth2Login(oauth2 -> oauth2
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/home",true)
+//                );
         return http.build();
     }
 }
--- backend/debt-management-api/src/main/java/com/debtmanager/debt_management_api/repository/UserRepository.java
+++ backend/debt-management-api/src/main/java/com/debtmanager/debt_management_api/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.debtmanager.debt_management_api.repository;
 import com.debtmanager.debt_management_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail();
 }
